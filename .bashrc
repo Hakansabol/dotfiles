5d2128b4185f2709d3f5f020741b99ca5a8f9da2
hakansabol <hakansabol@gmail.com>
2025-11-13 21:39:14 -0800

    C++ snip setup
    Added a couple of luasnips for C++ and added a warning when using cd reminding that zoxide should be used instead.

--- .bashrc
+++ .bashrc
@@ -4,10 +4,11 @@
 
 # If not running interactively, don't do anything
 [[ $- != *i* ]] && return
+PS1='[\u@\h \W]\$ '
 
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
-PS1='[\u@\h \W]\$ '
+alias cd='echo "cd is superceded by z" && cd'
 
 eval "$(zoxide init bash)"
 . "$HOME/.cargo/env"
--- nvim/lua/snips.lua
+++ nvim/lua/snips.lua
@@ -5,15 +5,24 @@ vim.api.nvim_create_user_command('Snips', function()
   print 'Sourced /snips.lua'
 end, {})
 
--- Snippets that work on all filetypes
+-- Snippets that are useful in every language
 ls.add_snippets('all', {
   ls.parser.parse_snippet('arch', 'Arch is the best!'),
 })
 
--- Snippets that work only on cs files (for Unity dev)
+-- Snippets for development in the Unity game engine
 ls.add_snippets('cs', {
   ls.parser.parse_snippet('gc', 'GetComponent<${1:T}>()'),
   ls.parser.parse_snippet('header', '[Serialize, Header("${1:Title}")]'),
   ls.parser.parse_snippet(',Start', 'public void Start() {\n\t$1\n}'),
   ls.parser.parse_snippet('summary', '/// <summary>\n/// $1\n/// </summary>'),
+})
+
+-- Snippets for general C++
+ls.add_snippets('cpp', {
+  ls.parser.parse_snippet(
+    ',cpp',
+    '#include <iostream>\nusing namespace std;\n\nint main(int argc, char* argv[])\n{\n	${1:cout << "Hello, world!";}\n	return 0;\n}\n'
+  ),
+  ls.parser.parse_snippet('fori', 'for (int i = 0; i < ${1:n}; i++)\n{\n\t$2\n}'),
 })
