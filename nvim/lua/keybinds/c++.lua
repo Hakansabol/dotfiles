6130f85d224159688e7ac39191f9d0d448393c53
hakansabol <hakansabol@gmail.com>
2025-12-05 11:03:29 -0800

    merge

--- nvim/lua/keybinds/c++.lua
+++ nvim/lua/keybinds/c++.lua
@@ -1,6 +1,6 @@
 -- Tools for working with c++
 function compile_and_run_cpp()
-  vim.api.nvim_command 'T gcc % -std=c++23'
+  vim.api.nvim_command 'T gcc % -std=c++23 -finput-charset=UTF-8'
   vim.api.nvim_command 'T ./a.out'
 end
 
