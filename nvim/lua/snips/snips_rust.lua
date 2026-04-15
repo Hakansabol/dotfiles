ba12a192004ca31183da95cd087b536883d282f0
hakansabol <hakansabol@gmail.com>
2026-04-14 21:58:45 -0700

    rust improvements
    shortened template, a couple snips improved, todo-comments re-enabled

--- nvim/lua/snips/snips_rust.lua
+++ nvim/lua/snips/snips_rust.lua
@@ -12,6 +12,32 @@ ls.add_snippets('rust', {
   -- code style snips
   ls.parser.parse_snippet('max', [[std::cmp::max($1,${2:0})]]),
   ls.parser.parse_snippet('min', [[std::cmp::min($1,${2:0})]]),
+  ls.parser.parse_snippet(
+    'zeratosthenes_nprimes',
+    [[/// ztemplate: Sieve of Eratosthenes [first n primes]
+/// cnt: the number of primes to find, starting with `1`.
+fn eratosthenes(cnt: usize) -> Vec<u64> {
+    assert!(cnt <= 1001000, "cnt too large for the sieve (> 1.001M)");
+    let mut sieve: Vec<bool> = vec![false; cnt * 16]; // the sieve could be a vector of u8 for memory efficiency.
+    let mut basin: Vec<u64> = vec![]; // the basin is the output where the primes fall into.
+    let mut point: u64 = 2; // The point is the point that the sieve is currently at.
+    while (basin.len() < cnt) {
+        // if this item is not flagged
+        if !sieve[point as usize] {
+            basin.push(point);
+            // update the sieve
+            let mut sieve_point = point;
+            while sieve_point < sieve.len() as u64 {
+                sieve[sieve_point as usize] = true;
+                sieve_point += point;
+            }
+        }
+        // next number
+        point += 1;
+    }
+    basin
+}]]
+  ),
   -- basic function declaration
   -- i(1): name
   -- c(2): toggle between &self implementation or nothing
@@ -50,8 +76,9 @@ ls.add_snippets('rust', {
     fmt(
       [[
 	let {} = {}.iter().map(|x| x.to_string()).collect::<Vec<String>>().join(" ");
+	println!("{{}}", {})
 		]],
-      { i(2, 'out'), i(1, 'nums') }
+      { i(2, 'out'), i(1, 'nums'), rep(2) }
     )
   ),
   -- variable declaration
--- scripts/rs_io_template.rs
+++ scripts/rs_io_template.rs
@@ -1,45 +1,22 @@
-/*
-This template is made by Naman Garg <naman.rustp@gmail.com>
-GitHub : https://github.com/namanlp
-GitLab : https://gitlab.com/namanlp
-Website : https://rustp.org
-
-You can visit https://rustp.org/basic-programs/basic-template/
-for understanding the template
-
-Feel free to copy the template, but not the solutions :D
-Thank You
- */
-
-#![allow(unused)]
-
-use std::io::stdin;
-
-fn take_int() -> i32 {
-    let mut input = String::new();
-    stdin().read_line(&mut input).unwrap();
-    return input.trim().parse().unwrap()
+// This template is adapted from Naman Garg's <naman.rustp@gmail.com> at https://rustp.org/basic-programs/basic-template/
+#[rustfmt::skip]
+#[allow(clippy::all, unused)]
+mod template {
+    use std::io::stdin;
+    pub fn take_int() -> i32{std::io::stdin().lines().next().unwrap().unwrap().trim().parse().unwrap()}
+    pub fn take_vector() -> Vec<i32>{std::io::stdin().lines().next().unwrap().unwrap().trim().split_whitespace().map(|x| x.parse().unwrap()).collect()}
+    pub fn take_string() -> Vec<char>{std::io::stdin().lines().next().unwrap().unwrap().trim().chars().collect()}
+    pub fn to_string(vec:Vec<char>) -> String{return vec.iter().collect::<String>();}
 }
 
-fn take_vector() -> Vec<i32> {
-    let mut input = String::new();
-    stdin().read_line(&mut input).unwrap();
-    let arr: Vec<i32> = input.trim().split_whitespace().map(|x| x.parse().unwrap()).collect();
-    return arr;
-}
-
-fn take_string() -> Vec<char> {
-    let mut input = String::new();
-    stdin().read_line(&mut input).unwrap();
-    let vec:Vec<char> = input.trim().chars().collect();
-    return vec;
-}
-fn to_string(vec:Vec<char>) -> String{return vec.iter().collect::<String>();}
+use template::*;
+//=========================================================
+//================== THE FUN BEGINS HERE ==================
+//=========================================================
 
 fn solve() {
-// ======================= Code Here =========================
+    //  TODO: CODE GOES HERE
 }
-
 
 pub fn main() {
     let t = take_int();
